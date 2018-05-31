515a3945ffe6f8d4c40fe6f4e69ddb8b9808f3d6
Keith Calvert <KeithCalvert06@gmail.com>
2018-05-31 17:10:08 +0200

    what has changed

--- AdReports.ps1
+++ AdReports.ps1
@@ -218,7 +218,7 @@ $fMain_BtnSelection.Add_Click({
 	{
 		1	{If ($fMain_LstOrdis.Items.Count -gt 0)
 				{		
-					$Global:AdOrdinateur = $fMain_lstUtils.SelectedItem.oOrdi
+					$Global:AdOrdinateur = $fMain_lstOrdis.SelectedItem.oOrdi
 					AdOrdiOuvrirFrm 
 				}
 	}
